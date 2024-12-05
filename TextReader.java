d6d6d1ddfaf68be1edbdaaa392828e3dae753e76
krisedtemakatt <90635215+krisedtemakatt@users.noreply.github.com>
2024-12-05 11:34:06 +0100

    Apply suggestions from code review
    yayyy
    
    Co-authored-by: Emma Haggren <90604582+emmahaggren@users.noreply.github.com>

--- TextReader.java
+++ TextReader.java
@@ -14,12 +14,11 @@ public class TextReader {
 	Map<String, ArrayList<Integer>> wordMap = new HashMap<>();
 
 	public void read(String fileName, String pattern) {
-
 		setUpWordMap(fileName);
 
 		Set<Integer> matchedLines = matchWords(wordMap, pattern);
 
-		printResults(matchedLines,lines);
+		printResults(matchedLines, lines);
 	}
 
 
@@ -32,19 +31,20 @@ public class TextReader {
 				lines.add(data);
 			}
 	
-			// adds all words in each line into wordList
-			int index = 0;
-			for (String line : lines) {
-				for (String word : line.split("\\s+")) {
-					if (!wordMap.containsKey(word)) {
-						wordMap.put(word, new ArrayList<>());
-						wordMap.get(word).add(index);
-					} else {
-						wordMap.get(word).add(index);
-					}
-				}
-				index++;
-			}
+                        // adds all words in each line into wordList
+                        int index = 0;
+                        for (String line : lines) {
+                            for (String rawWord : line.split("\\s+")) {
+                                String word = rawWord.replaceAll("[^a-zA-Z]", "");
+                                if (!wordMap.containsKey(word)) {
+                                    wordMap.put(word.replaceAll("[^a-zA-Z]", ""), new ArrayList<>());
+                                    wordMap.get(word).add(index);
+                                } else {
+                                    wordMap.get(word).add(index);
+                                }
+                            }
+                            index++;
+                        }
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("An error occurred.");
@@ -58,7 +58,7 @@ public class TextReader {
 		for(Map.Entry<String, ArrayList<Integer>> entry : words.entrySet() ) {
 			if (entry.getKey().trim().equals(pattern.trim())) {
 				for (int index : entry.getValue()) {
-					matchedLines.add(index);
+					matchedLines.add(index-1);
 				}
 				return matchedLines;
 			}
