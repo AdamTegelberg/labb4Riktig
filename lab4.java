d59636d0ab43ad0323dfcf6bd3e3f0a8dc477164
AdamTegelberg <90842046+AdamTegelberg@users.noreply.github.com>
2024-12-05 10:51:49 +0100

    Create lab4.java

---
+++ lab4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;  
+
+
+public class lab4 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("search <pattern> <file>");
+
+        String userInput = scanner.nextLine();
+        String pattern = userInput.split("\\s+")[1];
+        String fileName = userInput.split("\\s+")[2]; 
+
+        
+
+        TextReader reader = new TextReader();
+        reader.read(fileName, pattern);
+
+        scanner.close();
+        
+
+        
+    }
+
+}
